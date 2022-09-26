181d6ab5de92876c6e6b6582a9f8b47b42fd9bb3
edwinamessenger <edwinamessenger@icloud.com>
2022-09-26 14:11:32 +1000

    deleted everything and started again. loaded img files

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-var num1 = Math.floor(Math.random() * 255) + 1;
-var num2 = Math.floor(Math.random() * 255) + 1;
-var num3 = Math.floor(Math.random() * 255) + 1;
-var col1 = "rgb(" + num1 + ", " + num2 + ", " + num3 + ")";
-var col2 = "rgb(" + num2 + ", " + num3 + ", " + num1 + ")";
-var col3 = "rgb(" + num3 + ", " + num1 + ", " + num2 + ")";
 
 
-document.querySelector(".ball-one").style.backgroundColor = col1;
-document.querySelector(".ball-two").style.backgroundColor = col2;
-document.querySelector(".ball-three").style.backgroundColor = col3;
+//random rgb color generator
+//generate 3 random colors between/including 0-255, concatenate them into a string in rgb format: rgb(xx, xx, xx)
+
+function rgbNum() {
+    var num1 = Math.floor(Math.random() * 256);
+    var num2 = Math.floor(Math.random() * 256);
+    var num3 = Math.floor(Math.random() * 256);
+
+    return("rgb(" + num1 + ", " + num2 + ", " + num3 + ")");
+}
+
+
+$(".back").css("background-color", rgbNum);
+
+
+
+
 
